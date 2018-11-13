278bd0f5d62ac868eee5469cee7baa063ba0c836
caadmin <janel.brandon@coderacademy.edu.au>
2018-11-13 11:14:02 +1000

    solution for largest_number

---
+++ largest_number/02_largest_number.js
@@ -0,0 +1,34 @@
+/*
+Largest Number
+Write a method that will take an array of numbers
+and return whichever is the largest.
+
+Test your solution:
+mocha 02_largest_number.js
+(You'll need mocha installed first: `npm install mocha -g`)
+*/
+
+function largestNumber(arr) {
+    let largest = arr[0]
+    for (let i in arr) {
+        if (arr[i] > largest) {
+            largest = arr[i]
+        }
+    }
+    return largest
+}
+
+var assert = require('assert');
+
+describe('largestNumber', function () {
+    it('should return the largest number', function () {
+        assert.equal(10, largestNumber([5, -2, 10]));
+    })
+    it('should ignore invalid array entries', function () {
+        assert.equal(10, largestNumber([10, 10, 's']));
+    })
+    it('should return null if the array is empty', function () {
+        assert.equal(null, largestNumber([]))
+    })
+
+})
