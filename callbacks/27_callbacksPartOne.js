057345c76c03ab5b10749d4f95f7ed2a4590a694
caadmin <janel.brandon@coderacademy.edu.au>
2019-01-22 10:46:37 +1000

    added callbacks solution

---
+++ callbacks/27_callbacksPartOne.js
@@ -0,0 +1,30 @@
+/*
+Line 28 is calling 'sayGreetings' and handing it a callback 'sayNiceToMeetYou'
+Line 29 is calling 'sayGreetings' and handing it a callback 'sayGoodbye'
+
+Fix the 'sayGreetings' function so it runs the callback each time.
+
+DESIRED OUTPUT:
+Greetings!
+Nice to meet you!
+Greetings!
+Goodbye!
+
+Check your solution by running node 27_callbacksPartOne.js
+*/
+
+const sayGreetings = (greeting) => {
+    console.log('Greetings!')
+    greeting()
+}
+
+const sayNiceToMeetYou = () => {
+    console.log('Nice to meet you!')
+}
+
+const sayGoodbye = () => {
+    console.log('Goodbye!')
+}
+
+sayGreetings(sayNiceToMeetYou)
+sayGreetings(sayGoodbye)
