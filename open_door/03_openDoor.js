9311371c57b31dd549a6bf1e25ccf52057481abf
janelbrandon <janel.brandon@coderacademy.edu.au>
2019-08-29 13:30:54 +1000

    openDoor to use jest

---
+++ open_door/03_openDoor.js
@@ -0,0 +1,26 @@
+/*
+Open Door
+Return true if name is Simon and hasCoffee is true.
+Otherwise return false.
+Test your solution:
+npm test
+*/
+
+function openDoor(input) {
+    // Your code here
+    // Google JS syntax ;)
+    let open = false
+
+    while (!open &&
+        input.length > 0) {
+        a = input.pop()
+        if (a.name == 'Simon' && a.hasCoffee == true)
+            open = true
+    }
+
+    return open
+
+}
+module.exports = {
+    openDoor
+}
