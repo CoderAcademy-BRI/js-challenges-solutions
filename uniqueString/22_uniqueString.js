9ec198fb1b77d34120375cdc0878389549d0ee93
janelbrandon <janel.brandon@coderacademy.edu.au>
2019-09-01 15:01:40 +1000

    updated uniqueString to use jest

--- uniqueString/22_uniqueString.js
+++ uniqueString/22_uniqueString.js
@@ -5,41 +5,29 @@ Example:
 uniqueString(["alright", "isod", "Alright", "ALRIGHT"]) => "isod"
 
 Test your solution:
-mocha 22_uniqueString.js
+npm test
 */
 
 function getUnique(stringCounts) {
     for (let key of Object.keys(stringCounts)) {
         if (stringCounts[key].length == 1)
-            return stringCounts[key][0]
+            return stringCounts[key][0];
     }
 }
 
 function uniqueString(array) {
-    let unique = {}
+    let unique = {};
     for (let word of array) {
-        let orderedSetOfChars = [...new Set(word.toLowerCase())].sort().join('')
+        let orderedSetOfChars = [...new Set(word.toLowerCase())].sort().join('');
         if (Object.keys(unique).includes(orderedSetOfChars)) {
-            unique[orderedSetOfChars].push(word)
+            unique[orderedSetOfChars].push(word);
         } else {
-            unique[orderedSetOfChars] = [word]
+            unique[orderedSetOfChars] = [word];
         }
     }
-    return getUnique(unique)
+    return getUnique(unique);
 }
 
-const assert = require('assert')
-
-describe('Unique string challenge', function () {
-    it('Should return the unique string', function () {
-        assert.deepEqual(uniqueString(['aa', 'AaA', 'aaaa', 'bBbB', 'aAaA', 'a']), 'bBbB')
-    })
-
-    it('Should return the unique string when the array elements have mixed letters', function () {
-        assert.deepEqual(uniqueString(['kios', 'foo', 'ikos', 'ikso', 'kois', 'kiso']), 'foo')
-    })
-
-    it('Should return the unique string when it is the first one in the array arg', function () {
-        assert.deepEqual(uniqueString(['abc', ' ', '  ']), 'abc')
-    })
-})
+module.exports = {
+    uniqueString
+};
