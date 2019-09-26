08c354b06b06dee144a11afaafb6ffc11fa66ebe
janelbrandon <janel.brandon@coderacademy.edu.au>
2019-09-26 12:25:56 +1000

    added solution for multiplyValue

---
+++ multiplyValue/22_multiplyValue.js
@@ -0,0 +1,34 @@
+/*
+Create a function which will take a string or number value and multiply it a specific number of times. 
+
+The first parameter is the value to be multiplied. 
+
+The second parameter is the number of times the value should be multiplied.
+
+If the first parameter is not a number or string, the function should return null.
+
+If the second parameter is not a valid number the code should return null.
+
+Examples:
+multiplyValue(9,2) == 9 * 2 == 18
+multiplyValue("[alright]",3) == "[alright]" + "[alright]" + "[alright]" == "[alright][alright][alright]"
+multiplyValue(5,["kanye west"]) == null
+
+If your feeling confident add more tests below.
+*/
+
+exports.multiplyValue = (value, times) => {
+    // Your code here
+    let result = value * times;
+
+    if (Object.is(result, NaN)) {
+        result = null;
+        if (typeof value === "string") {
+            result = "";
+            for (let i = 1; i <= times; i++) {
+                result += value;
+            }
+        }
+    }
+    return result;
+}
